bb2dc4e5dd3ad96e71c02b1b147180727f855a8e
godarcher <joostgrunwald2001@gmail.com>
2022-09-27 15:19:54 +0200

    implementd div from scratch

--- assignment 4/AST.hs
+++ assignment 4/AST.hs
@@ -5,15 +5,16 @@ data Expr = Lit Integer | Add Expr Expr | Mul Expr Expr | Sub Expr Expr | Div Ex
 
 -- 4.6.2
 -- create division function
-div' :: Expr -> Expr -> Expr
-div' x y = `div` x y
+-- if y > x then return 0
+-- else return 1 + div (x-y) y
+div' x y = if y > x then Lit 0 else Add (Lit 1) (div' (Sub x y) y)
 
 --eval :: (Fractional a, Eq a) => Expr -> a -> Maybe a
 eval (Lit x) _ = Just x
 eval (Add x y) z = (+) <$> eval x z <*> eval y z
 eval (Mul x y) z = (*) <$> eval x z <*> eval y z
 eval (Sub x y) z = (-) <$> eval x z <*> eval y z
-eval (Div x y) z = if (eval y z) == Just 0 then Nothing else Just 3 --TODO replace just 3 with actual division
+eval (Div x y) z = if (eval y z) == Just 0 then Nothing else div' x y --TODO replace just 3 with actual division
 eval (VarX) z = Just z
 
 --or:
